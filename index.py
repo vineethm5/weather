73117eb7436e0068ba2c91bb77e12c7f4954e67f
vineethm5 <160957@sdmcujire.in>
2024-02-07 08:15:37 +0530

    adding congig file

--- index.py
+++ index.py
@@ -1,11 +1,24 @@
 from tkinter import *
 import tkinter
+from tkinter import messagebox
+from configparser import ConfigParser 
+import requests
 
 root=tkinter.Tk()
 root.geometry("230x230")
 root.resizable()
 root.title("Weather App")
 input_feild=StringVar()
+
+config_file="config.ini"
+config=ConfigParser()
+config.read(config_file)
+api_key=config['gfg']['api']
+url='http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+
+
+def getwether(city):
+    result=requests.get(url.format(city,api_key))
 
 def btn_click():
     pass
